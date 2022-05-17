5ee61119d912aa3c37082ba62830bd07a4cfee57
Dionprastt <96780245+Dionprastt@users.noreply.github.com>
2022-05-17 19:25:55 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,5 @@
 #(©)Codexbotz
+# @ItsMeYons
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Dionprastt/protectconten'>Click here</a>\n○ Channel : @File_Sharing_Support\n○ Owner Repo: @ItsMeYons</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
